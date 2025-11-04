47b169e8c2f83b361fe3a631877efc829f7aacf6
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2025-11-04 14:31:55 +0000

    Conversion Operators

---
+++ 01_basics/04_conversionOperators.js
@@ -0,0 +1,16 @@
+let str1 = "Hello";
+let str2 = " World";
+
+console.log(str1 + str2);
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+// use bracket instead
+
+console.log(+true);
+console.log(+"");
+
+let num = 3;
+console.log(num);
+++num;
+console.log(num);
