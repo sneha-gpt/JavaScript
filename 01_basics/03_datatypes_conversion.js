3b844dfc70096fc9f57f6c567e1db512a48f81ff
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2025-11-04 14:17:55 +0000

    DataType Conversion

---
+++ 01_basics/03_datatypes_conversion.js
@@ -0,0 +1,25 @@
+let s = "33abc";
+console.log(typeof(s));
+
+// let num = Number(s);
+// console.log(typeof(num));
+
+// s = undefined;
+// num = Number(s);
+// console.log(typeof(num));
+// console.log(num);
+
+
+// s = null;
+// num = Number(s);
+// console.log(typeof(num));
+// console.log(num);
+
+
+/*
+"33" => 33
+"33abs" => NaN
+*/
+
+let a = true;
+console.log(Number(a));
