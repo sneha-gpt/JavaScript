c387a2746b8dbe9cac11b8cf3b1722fa38a4fdac
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2025-11-05 13:12:11 +0000

    String

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,30 @@
+// let name = "Sneha";
+// let age = 21;
+
+// console.log(name + " " + age); //outdated
+// console.log(`Hello my name is ${name} and my age is ${age}`);
+
+// const name = new String("Sneha");
+// console.log(name);
+// console.log(name[0]);
+// console.log(name.charAt(2));
+// console.log(name.toUpperCase());
+// console.log(name.length);
+// console.log(name.indexOf('S'));
+// console.log(name.substring(1,3));
+// console.log(name.slice(-6, 3));
+
+let s = "    hijt     ";
+console.log(s.trim());
+console.log((s));
+
+console.log(s.includes("hi"));
+console.log(s.replace("hi", "ihg"));
+
+
+
+
+
+
+
+
