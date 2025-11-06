15f058876575ae2ca5b7e97fd8e3a0b678f183a5
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2025-11-06 00:56:46 +0000

    Number and Math

---
+++ 01_basics/08_number_math.js
@@ -0,0 +1,30 @@
+const score = 400;
+// console.log(score);
+
+const bal = new Number(100);
+// console.log(bal.toString().length);
+// console.log(bal.toFixed(2));
+
+const num = 1123.89;
+// console.log(num.toPrecision(3));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// -----------------------------Math-----------------------
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.sqrt(25));
+console.log(Math.round(23.67));
+console.log(Math.floor(5.7));
+
+console.log(Math.random());
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min)) + 1 + min);
+
+
