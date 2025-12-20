38ec829430e3637a7590dd0c06df0cf701b1ada5
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2025-12-20 09:01:36 +0000

    Datatypes

---
+++ Javascript/Lecture_1/DataTypes.js
@@ -0,0 +1,55 @@
+// Primitive (Store by Value)
+
+// string, number, boolean, null, undefined, symbol, bigint
+
+let s1 = "hello"
+let s2 = 'hello'
+let s3 = `hello`
+
+let n1 = 12
+let n2 = 12.3
+
+let a = true
+let b = false
+
+let selectedStudents = null //not selected till now 
+
+let u; //by default undefined 
+
+
+//Symbol //unique and immutable value 
+
+let u1 = Symbol("uid");
+let u2 = Symbol("uid");
+
+console.log(u1 === u2) //false 
+
+
+const user = {
+    name: "Sneha",
+    uid: 1
+}
+
+let id = Symbol("uid");
+user[id] = 2;
+
+console.log(user);
+
+// BigInt
+
+let xyz = 12n;
+console.log(xyz + 3n);
+
+// Reference (Store by Reference)
+
+// array, objects, functions
+
+let obj1 = {
+    name: "Sneha"
+}
+
+let obj2 = obj1;
+
+obj2["name"] = "harshita";
+console.log(obj1);
+console.log(obj2);
