8e95b4b80ab6d4a773ba552c6b97d23f18be189f
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2026-01-04 11:54:54 +0000

    Dev Dependeny

--- Backend/02_npm/01_npm.js
+++ Backend/02_npm/01_npm.js
@@ -1,12 +1,15 @@
-/* To install any package 
+/* 
 
+-> To install any package 
 npm install package_name or npm i package_name
 
 -> With specific version
-
 npm i package_name@version
 
 -> To uninstall
 npm uninstall package
+
+-> Dev Dependency
+npm i package_name --save-dev
 
 */
